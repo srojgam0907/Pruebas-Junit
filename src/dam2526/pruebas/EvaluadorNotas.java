d265e01787865039c3dc9ecf2690b83d23d190b3
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-22 09:05:30 +0200

    codigo principal

--- src/dam2526/pruebas/EvaluadorNotas.java
+++ src/dam2526/pruebas/EvaluadorNotas.java
@@ -2,9 +2,19 @@ package dam2526.pruebas;
 
 public class EvaluadorNotas {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public String clasificarNota(int nota) {
+        if (nota < 0 || nota > 10) {
+            throw new IllegalArgumentException("La nota debe estar entre 0 y 10.");
+        }
 
-	}
-
+        if (nota <= 4) {
+            return "Suspenso";
+        } else if (nota <= 6) {
+            return "Aprobado";
+        } else if (nota <= 8) {
+            return "Notable";
+        } else {
+            return "Sobresaliente";
+        }
+    } 
 }
