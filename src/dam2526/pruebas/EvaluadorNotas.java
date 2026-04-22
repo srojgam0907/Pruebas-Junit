734c366365564d440d8a1d56356372c4d180d401
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-22 08:47:38 +0200

    primer commit

---
+++ src/dam2526/pruebas/EvaluadorNotas.java
@@ -0,0 +1,10 @@
+package dam2526.pruebas;
+
+public class EvaluadorNotas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/dam2526/pruebas/test/EvaluadorNotasTest.java
@@ -0,0 +1,10 @@
+package dam2526.pruebas.test;
+
+public class EvaluadorNotasTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
