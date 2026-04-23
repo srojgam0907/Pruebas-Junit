f94ac89e2dd0ccf30f59946975758ada75d8db23
srojgam0907 <srojgam0907@g.educaand.es>
2026-04-23 13:00:45 +0200

    Practica 2, test parametrizados

---
+++ test/dam2526/pruebas/test/EvaluadorNotasParameterizedTest.java
@@ -0,0 +1,37 @@
+package dam2526.pruebas.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import dam2526.pruebas.EvaluadorNotas;
+
+public class EvaluadorNotasParameterizedTest {
+
+	@ParameterizedTest
+	@CsvSource({
+		"0, Suspenso",
+		"4, Suspenso",
+		"5, Aprobado",
+		"6, Aprobado",
+		"7, Notable",
+		"8, Notable",
+		"9, Sobresaliente",
+		"10, Sobresaliente"
+	})
+	void clasificarNota_devuelveCalificacionEsperada(int nota, String esperado) {
+		EvaluadorNotas evaluador= new EvaluadorNotas();
+		String resultado= evaluador.clasificarNota(nota);
+		assertEquals(esperado, resultado);
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints= {-1, 11})
+	void clasificarNota_fueraDeRango_lanzaExcepcion(int nota) {
+		EvaluadorNotas evaluador= new EvaluadorNotas();
+		assertThrows(IllegalArgumentException.class, () -> evaluador.clasificarNota(nota));
+	}
+}
